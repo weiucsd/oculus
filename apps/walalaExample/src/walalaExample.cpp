75d0ca0b1247aead1cd1f45c3fad93b92fa3867a
jasjuang <jason@ubuntu-nb.(none)>
2014-12-17 17:36:08 -0800

    Fix some bugs, now it for sure works.

--- apps/walalaExample/src/walalaExample.cpp
+++ apps/walalaExample/src/walalaExample.cpp
@@ -3,26 +3,9 @@
 
 int main()
 {
-	tinyxml2::XMLDocument doc;
+	VPL::walala walalaobj;
 
-	if (doc.LoadFile( "config.xml" ))
-	{
-		std::cout << "failed to load config file" << std::endl;
-		return -1;
-	}
-	else
-	{
-		std::cout <<  doc.FirstChildElement( "CONFIG" )->FirstChildElement( "SETTING" )->FirstChild()->ToText()->Value() << std::endl;
-	}
-
-	int x;
-	float y;
-
-	doc.FirstChildElement( "CONFIG" )->FirstChildElement( "VALUE" )->FirstChildElement( "x" )->QueryIntText( &x );
-	doc.FirstChildElement( "CONFIG" )->FirstChildElement( "VALUE" )->FirstChildElement( "y" )->QueryFloatText( &y );
-
-	std::cout << x << std::endl;
-	std::cout << y << std::endl; 
+	walalaobj.TestPrint(); 
 
 	return 0;
 }
--- modules/walala/src/walala.cpp
+++ modules/walala/src/walala.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "walala.h"
 
 namespace VPL {
 
