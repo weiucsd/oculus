dc18bad91e21f378cf48e8655774951a03dcc2e1
ifed-ucsd <ifedorov@eng.ucsd.edu>
2015-02-09 15:31:33 -0800

    Added cmake support for ovrvision

--- apps/walalaExample/src/walalaExample.cpp
+++ apps/walalaExample/src/walalaExample.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include "walala.h"
+#include "ovrvision.h"
+#include "OVR.h"
 
 int main(int argc, char *argv[])
 {
+	ovrHmd           HMD;
+	OVR::Ovrvision* g_pOvrvision;
+	g_pOvrvision = new OVR::Ovrvision();
+	/*
 	if(argc!=2)
 	{
 		std::cout << "please append the input path to your image behind ./walalaExample" << std::endl;
@@ -13,6 +19,10 @@ int main(int argc, char *argv[])
 
 	walalaobj.TestShowImage(argv[1]); 
 	walalaobj.TestPrint(); 
+	*/
+	cv::namedWindow("test window", cv::WINDOW_AUTOSIZE);
+	cv::imshow("test window", cv::imread(argv[1]));
+	cv::waitKey(0);
 
 	return 0;
 }
