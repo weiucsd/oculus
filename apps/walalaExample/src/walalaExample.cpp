6afbe6e3a7eaaf19edb62fd67cd0bd39cf6401d4
ifed-ucsd <ifedorov@eng.ucsd.edu>
2015-02-13 07:40:51 -0800

    modified walalaExample.cpp

--- apps/walalaExample/src/walalaExample.cpp
+++ apps/walalaExample/src/walalaExample.cpp
@@ -2,19 +2,15 @@
 #include "walala.h"
 #include "ovrvision.h"
 #include "OVR.h"
-<<<<<<< HEAD
 #include "hands.h"
 
 using namespace cv;
-=======
->>>>>>> ecb2cf375e0570eb4995eeb31cafe412405234a2
 
 int main(int argc, char *argv[])
 {
 	ovrHmd           HMD;
 	OVR::Ovrvision* g_pOvrvision;
 	g_pOvrvision = new OVR::Ovrvision();
-<<<<<<< HEAD
 	
 	Hands hands(500,500,10,false);
 
@@ -28,10 +24,6 @@ int main(int argc, char *argv[])
 	namedWindow("right", 1);
 
 	for (int ii = 0; ii < 202; ii++)
-=======
-	/*
-	if(argc!=2)
->>>>>>> ecb2cf375e0570eb4995eeb31cafe412405234a2
 	{
 		char filename[300];
 		sprintf(filename, "D:\\Documents\\VR-Hands-VPL\\Testing\\left\\file%d.png", ii);
@@ -43,19 +35,10 @@ int main(int argc, char *argv[])
 		hands.DetectHands(mat, matHands, 1);
 		hands.DetectHands(mat2, matHands2, 2);
 
-<<<<<<< HEAD
 		imshow("left", matHands);
 		imshow("right", matHands2);
 		waitKey(10);
 	}
-=======
-	walalaobj.TestShowImage(argv[1]); 
-	walalaobj.TestPrint(); 
-	*/
-	cv::namedWindow("test window", cv::WINDOW_AUTOSIZE);
-	cv::imshow("test window", cv::imread(argv[1]));
-	cv::waitKey(0);
->>>>>>> ecb2cf375e0570eb4995eeb31cafe412405234a2
 
 	return 0;
 }
